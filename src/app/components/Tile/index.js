b77ffe66629c003f76553f6cc7f257ae49eae310
Joaquin Acuña <misreads@gmail.com>
2022-03-12 12:06:55 -0300

    add tile component

--- src/app/components/Tile/index.js
+++ src/app/components/Tile/index.js
@@ -1,15 +1,24 @@
 import React from 'react';
+import { string } from 'prop-types';
 
 import styles from './styles.module.scss';
 
-function Tile() {
+function Tile({ letter }) {
     return (
       <div className={styles.container}>
         <button className={styles.tile}>
-          <span className={styles.letter}>P</span>
+          <span className={styles.letter}>{letter}</span>
         </button>
       </div>
     );
 }
+
+Tile.defaultProps = {
+    letter: 'p',
+};
+
+Tile.propTypes = {
+    letter: string,
+  };
 
 export default Tile;
