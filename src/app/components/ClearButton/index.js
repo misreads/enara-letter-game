276bb9203a5d821ac659ccc2bb25f7969bf88561
Joaquin Acuña <misreads@gmail.com>
2022-03-15 10:11:49 -0300

    add disabled when input is empty on clear button

--- src/app/components/ClearButton/index.js
+++ src/app/components/ClearButton/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { bool } from 'prop-types';
 
 import { cleanBoard, loadBoard } from '../../../redux/actions/gameActions';
@@ -8,6 +8,7 @@ import styles from './styles.module.scss';
 
 function ClearButton({ clearAll }) {
     const dispatch = useDispatch();
+    const enableButton = useSelector((state) => state.game.word).length > 0;
     const handleClick = () => {
         if (clearAll) {
             dispatch(loadBoard());
@@ -16,7 +17,11 @@ function ClearButton({ clearAll }) {
     };
     return (
       <div className={styles.container}>
-        <button onClick={handleClick} className={styles.button}>
+        <button
+          onClick={handleClick}
+          className={`${styles.button} ${(enableButton || clearAll) ? '' : styles['button-disabled']}`}
+          disabled={!(enableButton || clearAll)}
+        >
           <span className={styles.title}>
             {clearAll ? 'clear board' : 'clear word'}
           </span>
